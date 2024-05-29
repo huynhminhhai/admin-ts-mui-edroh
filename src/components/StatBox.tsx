9670c0964d983bd79c61bab7f0ada1af7aa015d4
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-05-29 13:06:10 +0700

    [CREATE] dashboard page

---
+++ src/components/StatBox.tsx
@@ -0,0 +1,46 @@
+import { Box, useTheme, Typography, colors } from "@mui/material";
+import { tokens } from "../theme";
+import ProgressCircle from "./ProgressCircle";
+
+const StatBox = (
+  { title, subtitle, icon, progress, increase }:
+    { title: string, subtitle: string, icon: React.ReactNode, progress: number, increase: any }
+) => {
+
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+
+  return (
+    <Box width="100%" m="0 30px">
+      <Box display="flex" justifyContent="space-between">
+        <Box>
+          {icon}
+          <Typography
+            variant="h4"
+            fontWeight="bold"
+            sx={{ color: colors.grey[100] }}
+          >
+            {title}
+          </Typography>
+        </Box>
+        <Box>
+          <ProgressCircle progress={progress} />
+        </Box>
+      </Box>
+      <Box display="flex" justifyContent="space-between" mt="2px">
+        <Typography variant="h5" sx={{ color: colors.greenAccent[500] }}>
+          {subtitle}
+        </Typography>
+        <Typography
+          variant="h5"
+          fontStyle="italic"
+          sx={{ color: colors.greenAccent[600] }}
+        >
+          {increase}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default StatBox
